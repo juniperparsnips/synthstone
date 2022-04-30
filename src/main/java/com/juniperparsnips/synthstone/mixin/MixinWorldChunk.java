2c6f547c1b6f7ac49af21f18fe85ebd7f9a7ab33
Juniper Parsons <juniperparsnips@gmail.com>
2022-04-29 22:40:56 -0400

    Think about litematica conflicts

--- src/main/java/com/juniperparsnips/synthstone/mixin/MixinWorldChunk.java
+++ src/main/java/com/juniperparsnips/synthstone/mixin/MixinWorldChunk.java
@@ -15,8 +15,7 @@ public abstract class MixinWorldChunk {
                     target = "Lnet/minecraft/world/chunk/ChunkSection;getBlockState(III)" +
                             "Lnet/minecraft/block/BlockState;")),
             at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;isClient:Z", ordinal = 0))
-    private boolean redirectIsRemote(World world)
-    {
+    private boolean redirectIsRemote(World world) {
         return ((IWorldUpdateSuppressor) world).getShouldPreventBlockUpdates() || world.isClient;
     }
 }
